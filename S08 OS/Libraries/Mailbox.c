9a7a67d20bc6c0d73c73e260678c36b494edbf63
VictorGreen <vdgreen_10@hotmail.com>
2017-09-08 12:12:02 -0500

    Add files via upload

--- S08 OS/Libraries/Mailbox.c
+++ S08 OS/Libraries/Mailbox.c
@@ -5,9 +5,28 @@
 
 Mailbox_t semaphore[MAILBOXES];
 
-void CreateMailbox(unsigned char mailbox_id, unsigned char consumer_id, unsigned char producer_id){
+voidCreateMailbox(unsigned char mailbox_id, unsigned char consumer_id, unsigned char producer_id){
 	semaphore[mailbox_id].Mailbox_id = mailbox_id;
 	semaphore[mailbox_id].Consumer_id = (consumer_id >= 1 && consumer_id < 10) ? consumer_id : 0x01;
 	semaphore[mailbox_id].Productor_id = (producer_id > 0 && producer_id < 10 && producer_id != consumer_id) ? producer_id : 0x02;
 	semaphore[mailbox_id].Flag = 0x00;	/* No hay mensaje por default */
+}
+
+void SetMail(unsigned char mailbox_id, unsigned char message){
+	if(semaphore[mailbox_id].Flag == 0x00){
+		if(semaphore[mailbox_id].Productor_id == running_task){
+			semaphore[mailbox_id].Message = message;
+			semaphore[mailbox_id].Flag = 0x01;
+		}
+	}
+}
+
+unsigned char GetMail(unsigned char mailbox_id){
+	if(semaphore[mailbox_id].Flag == 0x01){
+		if(semaphore[mailbox_id].Consumer_id == running_task){
+			semaphore[mailbox_id].Flag = 0x00;
+			return message;
+		}
+	}
+	return 0;
 }
