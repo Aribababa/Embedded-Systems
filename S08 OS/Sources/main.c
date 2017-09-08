967506d8d4d8359d636cc0913428ec3f74ba7734
Rafael López <aribababa@outlook.com>
2017-09-08 11:28:48 -0500

    Add files via upload

--- S08 OS/Sources/main.c
+++ S08 OS/Sources/main.c
@@ -1,60 +1,90 @@
 #include <hidef.h> /* for EnableInterrupts macro */
 #include "derivative.h" /* include peripheral declarations */
-#include <OS.h>
+#include "OSEKduino.h"
 
-DeclareTask(TASK_A);
-DeclareTask(TASK_B);
-DeclareTask(TASK_C);
-DeclareTask(TASKERINO);
+unsigned char PWM_01_flag;
+unsigned char PWM_02_flag;
+unsigned char PWM_03_flag;
+unsigned char PWM_04_flag;
 
+unsigned char duty_cycle_pwm01 = 90;
+unsigned char duty_cycle_pwm02 = 5;
+unsigned char duty_cycle_pwm03 = 40;
+unsigned char duty_cycle_pwm04 = 66;
+
+/* Para los PWM */
+DeclareTask(PWM_01);	/* PWM con Duty Cicle de 20% */
+DeclareTask(PWM_02);
+DeclareTask(PWM_03);
+DeclareTask(PWM_04);
 
 void main(void) {
-	KBISC = 0x02;
-	KBIPE = 0x08;
-	KBIES = 0x00;
-	
-	CreateTask(1,0 , AUTOSTART, &TASK_A);
-	CreateTask(2, 1, NO_AUTOSTART, &TASK_B);
-	CreateTask(3, 2, NO_AUTOSTART, &TASK_C);
-	CreateTask(4, 3, NO_AUTOSTART, &TASKERINO);
-	
+	PTBDD = 0xF0;	/* Declaramos */
 	SOPT1 = 0x52;
+	
+	CreateTask(1,10, NO_AUTOSTART, &PWM_01);
+	CreateTask(2,11, NO_AUTOSTART, &PWM_02);
+	CreateTask(3,12, NO_AUTOSTART, &PWM_03);
+	CreateTask(4,13, NO_AUTOSTART, &PWM_04);
+	
+	SetAlarm(1, 1, 1, REPETITIVE);
+	SetAlarm(2, 2, 1, REPETITIVE);
+	SetAlarm(3, 3, 1, REPETITIVE);
+	SetAlarm(4, 4, 1, REPETITIVE);
+	
 	EnableInterrupts;
+	init_OS();
 	
-	
-	Init_OS();
-
-  for(;;){
-	  
-  }
-
+	for(;;);
 }
 
-TASK(TASK_A){
-	PTADD_PTADD0 = 1;
-	
-	__asm NOP;
-	ActivateTask(2);
-	__asm NOP;
+/*
+ * 
+ * Formula de %D = 100*(T(on)/Periodo)
+ * */
+
+TASK(PWM_01){
+	PTBD_PTBD7 = ~PTBD_PTBD7;
+	if(PWM_01_flag%2){
+		SetAlarm(1, 1, 100 - (duty_cycle_pwm01), REPETITIVE);
+	} else {
+		SetAlarm(1, 1, (duty_cycle_pwm01), REPETITIVE);
+	}
+	PWM_01_flag++;
+	TerminateTask();
+	//SetAlarm(1, 1, 8, REPETITIVE);
+}
+
+TASK(PWM_02){
+	PTBD_PTBD6 = ~PTBD_PTBD6;
+	if(PWM_02_flag%2){
+		SetAlarm(2, 2, 100 - (duty_cycle_pwm02), REPETITIVE);
+	} else {
+		SetAlarm(2, 2,(duty_cycle_pwm02), REPETITIVE);
+	}
+	PWM_02_flag++;
 	TerminateTask();
 }
 
-TASK(TASK_B){
-	__asm NOP;
-	ChainTask(3);
-}
-
-TASK(TASK_C){
-	__asm NOP;
+TASK(PWM_03){
+	PTBD_PTBD5 = ~PTBD_PTBD5;
+	if(PWM_03_flag%2){
+		SetAlarm(3, 3, 100 - (duty_cycle_pwm03), REPETITIVE);
+	} else {
+		SetAlarm(3, 3, duty_cycle_pwm03, REPETITIVE);
+	}
+	PWM_03_flag++;
 	TerminateTask();
 }
 
-TASK(TASKERINO){
-	__asm NOP;
+TASK(PWM_04){
+	
+	PTBD_PTBD4 = ~PTBD_PTBD4;
+	if(PWM_04_flag%2){
+		SetAlarm(4, 4, 100 - (duty_cycle_pwm04), REPETITIVE);
+	} else {
+		SetAlarm(4, 4, duty_cycle_pwm04, REPETITIVE);
+	}
+	PWM_04_flag++;
 	TerminateTask();
-}
-
-ISR(Keyboard_ISR){
-	KBISC_KBACK = 0x01;
-	ActivateTask_ISR(3);
 }
