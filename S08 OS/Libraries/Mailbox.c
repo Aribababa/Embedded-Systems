5ebaaaffa9215dd21c48e2cea45f6c63c5e9e3c4
VictorGreen <vdgreen_10@hotmail.com>
2017-09-12 20:46:54 -0500

    Add files via upload

--- S08 OS/Libraries/Mailbox.c
+++ S08 OS/Libraries/Mailbox.c
@@ -5,7 +5,7 @@
 
 Mailbox_t semaphore[MAILBOXES];
 
-void CreateMailbox(unsigned char mailbox_id, unsigned char consumer_id, unsigned char producer_id){
+voidCreateMailbox(unsigned char mailbox_id, unsigned char consumer_id, unsigned char producer_id){
 	semaphore[mailbox_id].Mailbox_id = mailbox_id;
 	semaphore[mailbox_id].Consumer_id = (consumer_id >= 1 && consumer_id < 10) ? consumer_id : 0x01;
 	semaphore[mailbox_id].Productor_id = (producer_id > 0 && producer_id < 10 && producer_id != consumer_id) ? producer_id : 0x02;
@@ -29,4 +29,13 @@ unsigned char GetMail(unsigned char mailbox_id){
 		}
 	}
 	return 0;
+}
+
+void WaitMessage(unsigned char semaforo){
+	if(semaphore[semaforo].Flag == 0){
+		task[running_task].State = WAITING;
+		SaveContext();
+		__asm ais #3;
+		Schedule();	
+	}
 }
